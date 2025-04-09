1ed05384cce0db38dc41632f7f828f16d0e58bac
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2025-04-09 10:41:39 +0100

    initial commit

---
+++ src/main/java/com/example/aiprojectapi/AiProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.aiprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiProjectApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aiprojectapi/AiProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
